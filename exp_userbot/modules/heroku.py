c1a391686283b568e90c3cee9ed75a10789dc57d
Kennedy <87056837+KennedyProject@users.noreply.github.com>
2021-09-24 11:44:24 +0700

    try

---
+++ exp_userbot/modules/heroku.py
@@ -0,0 +1,102 @@
+
+# Copyright (C) 2020 Adek Maulana.
+# All rights reserved.
+"""
+   Heroku manager for your userbot
+"""
+
+import math
+
+import aiohttp
+import heroku3
+
+from exp_userbot import NEXAUB, CMD_HELP
+from config import Config
+from exp_userbot.core.main_cmd import expub_on_cmd, e_or_r
+
+heroku_api = "https://api.heroku.com"
+if HEROKU_APP_NAME is not None and HEROKU_API_KEY is not None:
+    Heroku = heroku3.from_key(HEROKU_API_KEY)
+    app = Heroku.app(HEROKU_APP_NAME)
+    heroku_var = app.config()
+else:
+    app = None
+
+"""
+    Check account quota, remaining quota, used quota, used app quota
+"""
+
+
+@expub_on_cmd(command="update", modlue=mod_file)
+async def dyno_usage(dyno):
+    """Get your account Dyno Usage."""
+    if app is None:
+        return await dyno.edit(
+            "`[HEROKU]\nPlease setup your`  "
+            "**HEROKU_APP_NAME** and ***HEROKU_API_KEY**."
+        )
+    await dyno.edit("`Getting Information...`")
+    useragent = (
+        "Mozilla/5.0 (Linux; Android 10; SM-G975F) "
+        "AppleWebKit/537.36 (KHTML, like Gecko) "
+        "Chrome/81.0.4044.117 Mobile Safari/537.36"
+    )
+    user_id = Heroku.account().id
+    headers = {
+        "User-Agent": useragent,
+        "Authorization": f"Bearer {HEROKU_API_KEY}",
+        "Accept": "application/vnd.heroku+json; version=3.account-quotas",
+    }
+    path = "/accounts/" + user_id + "/actions/get-quota"
+    async with aiohttp.ClientSession() as session:
+        async with session.get(heroku_api + path, headers=headers) as r:
+            if r.status != 200:
+                await dyno.client.send_message(
+                    dyno.chat_id, f"`{r.reason}`", reply_to=dyno.id
+                )
+                await dyno.edit("`Can't get information...`")
+                return False
+            result = await r.json()
+            quota = result["account_quota"]
+            quota_used = result["quota_used"]
+
+            """ - User Quota Limit and Used - """
+            remaining_quota = quota - quota_used
+            percentage = math.floor(remaining_quota / quota * 100)
+            minutes_remaining = remaining_quota / 60
+            hours = math.floor(minutes_remaining / 60)
+            minutes = math.floor(minutes_remaining % 60)
+
+            """ - User App Used Quota - """
+            Apps = result["apps"]
+            for apps in Apps:
+                if apps.get("app_uuid") == app.id:
+                    AppQuotaUsed = apps.get("quota_used") / 60
+                    AppPercentage = math.floor(apps.get("quota_used") * 100 / quota)
+                    break
+            else:
+                AppQuotaUsed = 0
+                AppPercentage = 0
+
+            AppHours = math.floor(AppQuotaUsed / 60)
+            AppMinutes = math.floor(AppQuotaUsed % 60)
+
+            await dyno.edit(
+                "**Dyno Usage**:\n\n"
+                f"-> `Dyno usage for`  **{app.name}**:\n"
+                f"     •  **{AppHours} hour(s), "
+                f"{AppMinutes} minute(s)  -  {AppPercentage}%**"
+                "\n\n"
+                "-> `Dyno hours quota remaining this month`:\n"
+                f"     •  **{hours} hour(s), {minutes} minute(s)  "
+                f"-  {percentage}%**"
+            )
+            return True
+
+
+CMD_HELP.update(
+    {
+        "heroku": ">.`usage`"
+        "\nUsage: Check your heroku dyno hours remaining"
+    }
+)
