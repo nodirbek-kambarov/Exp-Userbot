4b09901d031960720da79b7d4970bcda997c94b9
Kennedy <87056837+KennedyProject@users.noreply.github.com>
2021-09-24 15:12:57 +0700

    Update ub_updater.py

--- exp_userbot/modules/ub_updater.py
+++ exp_userbot/modules/ub_updater.py
@@ -55,158 +55,175 @@ async def updateme_requirements():
         return repr(e)
 
 
-@expub_on_cmd(command="update", modlue=mod_file)
-async def upstream(client, message):
-    status = await e_or_r(expub_message=message, msg_text=f"`Checking For Updates from` [Exp-Userbot]({UPSTREAM_REPO_URL}) `Repo...`")
-    conf = get_arg(message)
-    off_repo = UPSTREAM_REPO_URL
-    txt = "`Oops! Updater Can't Continue...`"
-    txt += "\n\n**LOGTRACE:**\n"
-    try:
-        repo = Repo()
-    except NoSuchPathError as error:
-        await status.edit(f"{txt}\n`directory {error} is not found`")
-        repo.__del__()
-        return
-    except GitCommandError as error:
-        await status.edit(f"{txt}\n`Early failure! {error}`")
-        repo.__del__()
-        return
-    except InvalidGitRepositoryError as error:
-        if conf != "now":
-            pass
-        repo = Repo.init()
-        origin = repo.create_remote("upstream", off_repo)
-        origin.fetch()
-        repo.create_head("main", origin.refs.main)
-        repo.heads.main.set_tracking_branch(origin.refs.main)
-        repo.heads.main.checkout(True)
-    ac_br = repo.active_branch.name
-    if ac_br != "main":
-        await status.edit(
-            f"`Can't update your Exp-Userbot becuase you're using a custom branch.` \n\n**Default Branch:** `main` \n**You are on:** `{ac_br}` \n`Please change to main branch.`"
-        )
-        repo.__del__()
-        return
-    try:
-        repo.create_remote("upstream", off_repo)
-    except BaseException:
-        pass
-    ups_rem = repo.remote("upstream")
-    ups_rem.fetch(ac_br)
-    if "now" not in conf:
-        try:
-            changelog = await gen_chlog(repo=repo, diff=f"{ac_br}")
-        except:
-            pass
-        if changelog:
-            changelog_str = f"**New Updates available for Exp Userbot** \n\n**Branch:** [{ac_br}]({UPSTREAM_REPO_URL}/tree/{ac_br}) \n\n**Changelog (last 10):** \n\n{changelog}"
-            if len(changelog_str) > 4096:
-                await status.edit("`Changelog is too big, view the file to see it.`")
-                file = open("EXPUB_git_commit_log.txt", "w+")
-                file.write(changelog_str)
-                file.close()
-                await EXPUB.send_document(
-                    message.chat.id,
-                    "EXPUB_git_commit_log.txt",
-                    caption=f"Do `{Config.CMD_PREFIX}update now` to update your Exp-Userbot",
-                    reply_to_message_id=status.message_id,
-                )
-                remove("EXPUB_git_commit_log.txt")
-            else:
-                return await status.edit(
-                    f"{changelog_str}\n\nDo `.update now` to update your Exp-Userbot",
-                    disable_web_page_preview=True,
-                )
-        else:
-            await status.edit(
-                f"\n**Exp-Userbot is Uptodate with** [{ac_br}]({UPSTREAM_REPO_URL}/tree/{ac_br})**\n",
-                disable_web_page_preview=True,
-            )
-            repo.__del__()
-        return
-    if Config.HEROKU_API_KEY is not None:
-        heroku = heroku3.from_key(Config.HEROKU_API_KEY)
+async def deploy(event, repo, ups_rem, ac_br, txt):
+    if HEROKU_API_KEY is not None:
+        import heroku3
+        heroku = heroku3.from_key(HEROKU_API_KEY)
         heroku_app = None
         heroku_applications = heroku.apps()
-        if not Config.HEROKU_APP_NAME:
-            await status.edit(
-                "**Error:** `Please add HEROKU_APP_NAME variable to continue update!`"
+        if HEROKU_APP_NAME is None:
+            await event.edit(
+                '`[HEROKU]: Harap Siapkan Variabel` **HEROKU_APP_NAME** `'
+                ' untuk dapat deploy perubahan terbaru dari 𝐊𝐄𝐍-𝐔𝐁𝐎𝐓`'
             )
             repo.__del__()
             return
         for app in heroku_applications:
-            if app.name == Config.HEROKU_APP_NAME:
+            if app.name == HEROKU_APP_NAME:
                 heroku_app = app
                 break
         if heroku_app is None:
-            await status.edit(
-                f"{txt}\n`Invalid Heroku credentials.`"
+            await event.edit(
+                f'{txt}\n`Kredensial Heroku tidak valid untuk deploy KEN-UBOT dyno.`'
             )
-            repo.__del__()
-            return
-        await status.edit(
-            "`Userbot Dyno Build is in Progress! Please wait...`"
-        )
+            return repo.__del__()
+        await event.edit('`[HEROKU]:'
+                         '\nSedang MengUpdate 𝐊𝐄𝐍-𝐔𝐁𝐎𝐓, Mohon Menunggu 5-7 Menit`'
+                         )
         ups_rem.fetch(ac_br)
         repo.git.reset("--hard", "FETCH_HEAD")
         heroku_git_url = heroku_app.git_url.replace(
-            "https://", "https://api:" + Config.HEROKU_API_KEY + "@"
-        )
+            "https://", "https://api:" + HEROKU_API_KEY + "@")
         if "heroku" in repo.remotes:
             remote = repo.remote("heroku")
             remote.set_url(heroku_git_url)
         else:
             remote = repo.create_remote("heroku", heroku_git_url)
         try:
-            remote.push(refspec=f"HEAD:refs/heads/{ac_br}", force=True)
+            remote.push(refspec="HEAD:refs/heads/master", force=True)
         except GitCommandError as error:
-            pass
-        await status.edit("`Successfully Updated!` \n**Restarting Now...**")
+            await event.edit(f'{txt}\n`Terjadi Kesalahan Di Log:\n{error}`')
+            return repo.__del__()
+        build = app.builds(order_by="created_at", sort="desc")[0]
+        if build.status == "failed":
+            await event.edit(
+                "`Build Gagal!\n" "Dibatalkan atau ada beberapa kesalahan...`"
+            )
+            await asyncio.sleep(5)
+            return await event.delete()
+        else:
+            await event.edit("`KEN-UBOT Berhasil Di Deploy!\n" "Restarting, Mohon Tunggu Sebentar.....`")
+            await asyncio.sleep(15)
+            await event.delete()
     else:
-        try:
-            ups_rem.pull(ac_br)
-        except GitCommandError:
-            repo.git.reset("--hard", "FETCH_HEAD")
-        await updateme_requirements()
-        await status.edit("`Successfully Updated!` \n**Restarting Now...**",)
-        args = [sys.executable, "-m" "exp_userbot"]
-        execle(sys.executable, *args, environ)
-        return
+        await event.edit('`[HEROKU]:'
+                         '\nHarap Siapkan Variabel` **HEROKU_API_KEY** `.`'
+                         )
+        await asyncio.sleep(10)
+        await event.delete()
+    return
 
-# Userbot restart module
-async def restart_expub():
-    if Config.HEROKU_API_KEY is not None:
-        heroku_conn = heroku3.from_key(Config.HEROKU_API_KEY)
-        server = heroku_conn.app(Config.HEROKU_APP_NAME)
-        server.restart()
-    else:
-        args = [sys.executable, "-m" "exp_userbot"]
-        execle(sys.executable, *args, environ)
-        exit()
 
-@expub_on_cmd(command="restart", modlue=mod_file)
-async def restart(client, message):
-    restart_msg = await e_or_r(expub_message=message, msg_text="`Processing...`")
-    await restart_msg.edit("`Exp-Userbot is restarting! Please wait...`")
+async def update(event, repo, ups_rem, ac_br):
     try:
-        await restart_expub()
-    except Exception as e:
-        await restart_msg.edit(f"**Error:** `{e}`")
+        ups_rem.pull(ac_br)
+    except GitCommandError:
+        repo.git.reset("--hard", "FETCH_HEAD")
+    await update_requirements()
+    await event.edit('**𝐊𝐄𝐍-𝐔𝐁𝐎𝐓** `Berhasil Di Update!`')
+    await asyncio.sleep(1)
+    await event.edit('**𝐊𝐄𝐍-𝐔𝐁𝐎𝐓** `Di Restart....`')
+    await asyncio.sleep(1)
+    await event.edit('`Mohon Menunggu Beberapa Detik.`')
+    await asyncio.sleep(10)
+    await event.delete()
+
+    # Spin a new instance of bot
+    args = [sys.executable, "-m", "exp-userbot"]
+    execle(sys.executable, *args, environ)
+    return
 
 
-@expub_on_cmd(command="logs", modlue=mod_file)
-async def log(client, message):
+@expub_on_cmd(command="update", modlue=mod_file)
+async def upstream(event):
+    "For .update command, check if the bot is up to date, update if specified"
+    await event.edit("`Mengecek Pembaruan, Silakan Menunggu....`")
+    conf = event.pattern_match.group(1)
+    off_repo = UPSTREAM_REPO_URL
+    force_update = False
     try:
-        await e_or_r(expub_message=message, msg_text="`Getting Logs`")
-        heroku_conn = heroku3.from_key(Config.HEROKU_API_KEY)
-        server = heroku_conn.get_app_log(Config.HEROKU_APP_NAME, dyno='worker', lines=100, source='app', timeout=100)
-        f_logs = server
-        if len(f_logs) > 4096:
-            file = open("logs.txt", "w+")
-            file.write(f_logs)
+        txt = "`Mohon Maaf, Pembaruan Tidak Dapat Di Lanjutkan Karna "
+        txt += "Beberapa Masalah Terjadi`\n\n**LOGTRACE:**\n"
+        repo = Repo()
+    except NoSuchPathError as error:
+        await event.edit(f'{txt}\n`Directory {error} Tidak Dapat Di Temukan`')
+        return repo.__del__()
+    except GitCommandError as error:
+        await event.edit(f'{txt}\n`Gagal Awal! {error}`')
+        return repo.__del__()
+    except InvalidGitRepositoryError as error:
+        if conf is None:
+            return await event.edit(
+                f"`Sayangnya, Directory {error} Tampaknya Bukan Dari Repo."
+                "\nTapi Kita Bisa Memperbarui Paksa Userbot Menggunakan .update now.`"
+            )
+        repo = Repo.init()
+        origin = repo.create_remote("upstream", off_repo)
+        origin.fetch()
+        force_update = True
+        repo.create_head("master", origin.refs.master)
+        repo.heads.master.set_tracking_branch(origin.refs.master)
+        repo.heads.master.checkout(True)
+
+    ac_br = repo.active_branch.name
+    if ac_br != UPSTREAM_REPO_BRANCH:
+        await event.edit(
+            '**[UPDATER]:**\n'
+            f'`Looks like you are using your own custom branch ({ac_br}). '
+            'in that case, Updater is unable to identify '
+            'which branch is to be merged. '
+            'please checkout to any official branch`')
+        return repo.__del__()
+    try:
+        repo.create_remote('upstream', off_repo)
+    except BaseException:
+        pass
+
+    ups_rem = repo.remote('upstream')
+    ups_rem.fetch(ac_br)
+
+    changelog = await gen_chlog(repo, f'HEAD..upstream/{ac_br}')
+
+    if changelog == '' and force_update is False:
+        await event.edit(
+            f'\n☄️ 𝐊𝐄𝐍-𝐔𝐁𝐎𝐓 Sudah Versi Terbaru\n')
+        await asyncio.sleep(15)
+        await event.delete()
+        return repo.__del__()
+
+    if conf is None and force_update is False:
+        changelog_str = f'**Pembaruan Untuk 𝐊𝐄𝐍-𝐔𝐁𝐎𝐓 :\n\n➣ Pembaruan Data :**\n`{changelog}`'
+        if len(changelog_str) > 4096:
+            await event.edit("`Changelog Terlalu Besar, Lihat File Untuk Melihatnya.`")
+            file = open("output.txt", "w+")
+            file.write(changelog_str)
             file.close()
-            await EXPUB.send_document(message.chat.id, "logs.txt", caption=f"Logs of `{Config.HEROKU_APP_NAME}`")
-            remove("logs.txt")
-    except Exception as e:
-        await message.edit(f"**Error:** `{e}`")
+            await event.client.send_file(
+                event.chat_id,
+                "output.txt",
+                reply_to=event.id,
+            )
+            remove("output.txt")
+        else:
+            await event.edit(changelog_str)
+        return await event.respond('**Perintah Untuk Update, Sebagai Berikut.**\n Command: >`.update now`\n Command: >`.update deploy`\n\n__Untuk Meng Update Fitur 𝐊𝐄𝐍-𝐔𝐁𝐎𝐓.__')
+
+    if force_update:
+        await event.edit(
+            '`Sinkronisasi Paksa Ke Kode Userbot Stabil Terbaru, Harap Tunggu .....`')
+    else:
+        await event.edit('` Proses Update 𝐊𝐄𝐍-𝐔𝐁𝐎𝐓, Loading....1%`')
+        await event.edit('` Proses Update 𝐊𝐄𝐍-𝐔𝐁𝐎𝐓, Loading....20%`')
+        await event.edit('` Proses Update 𝐊𝐄𝐍-𝐔𝐁𝐎𝐓, Loading....35%`')
+        await event.edit('` Proses Update 𝐊𝐄𝐍-𝐔𝐁𝐎𝐓, Loading....77%`')
+        await event.edit('` Proses Update 𝐊𝐄𝐍-𝐔𝐁𝐎𝐓, Updating...90%`')
+        await event.edit('` Proses Update 𝐊𝐄𝐍-𝐔𝐁𝐎𝐓, Mohon Tunggu Sebentar....100%`')
+    if conf == "now":
+        await update(event, repo, ups_rem, ac_br)
+        await asyncio.sleep(10)
+        await event.delete()
+    elif conf == "deploy":
+        await deploy(event, repo, ups_rem, ac_br, txt)
+        await asyncio.sleep(10)
+        await event.delete()
+    return
